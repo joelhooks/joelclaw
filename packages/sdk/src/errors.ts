51bfd888224bf9e6e5e7ac8c2deeeacecff90e9e
Joel Hooks <joelhooks@gmail.com>
2026-03-02 19:44:11 -0800

    Revert "cleanup: remove spurious packages/sdk created by codex"
    This reverts commit f117ae76270cf86dc83d4572afdcbf5b713562da.

---
+++ packages/sdk/src/errors.ts
@@ -0,0 +1,39 @@
+import type { JoelclawEnvelope } from "./types"
+
+export class JoelclawProcessError extends Error {
+  readonly bin: string
+  readonly args: readonly string[]
+  readonly exitCode: number | null
+  readonly signal: NodeJS.Signals | null
+  readonly stdout: string
+  readonly stderr: string
+
+  constructor(input: {
+    message: string
+    bin: string
+    args: readonly string[]
+    exitCode: number | null
+    signal: NodeJS.Signals | null
+    stdout: string
+    stderr: string
+  }) {
+    super(input.message)
+    this.name = "JoelclawProcessError"
+    this.bin = input.bin
+    this.args = input.args
+    this.exitCode = input.exitCode
+    this.signal = input.signal
+    this.stdout = input.stdout
+    this.stderr = input.stderr
+  }
+}
+
+export class JoelclawEnvelopeError<TResult = unknown> extends Error {
+  readonly envelope: JoelclawEnvelope<TResult>
+
+  constructor(envelope: JoelclawEnvelope<TResult>) {
+    super(envelope.error?.message || `joelclaw command failed: ${envelope.command}`)
+    this.name = "JoelclawEnvelopeError"
+    this.envelope = envelope
+  }
+}
